677a3770c86e74c01042e75594220048e9eca522
tonytega <aakpovire@gmail.com>
2023-04-01 00:03:41 +0100

    added app key in config/admin

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','vcMetV4+063a15MFKA+P3g=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','PATJmt5bhYnqVSF4ZydbEg=='),
   },
 });
