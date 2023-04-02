f5ef785adb7f667ff35f9c58b7ef1c73dbcdd8e0
tonytega <aakpovire@gmail.com>
2023-04-02 20:46:05 +0100

    removed brainwash

--- src/api/brainwash/controllers/brainwash.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * brainwash controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::brainwash.brainwash');
--- src/api/brainwash/services/brainwash.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * brainwash service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::brainwash.brainwash');
